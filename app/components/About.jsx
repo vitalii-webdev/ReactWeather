31b732fda33aa75dc864e8b8603adfd69b2f409b
Vitalii Chernyshenko <Vitalii Chernyshenko>
2017-01-17 22:14:55 +0200

    Styling to about page added

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,19 @@
 
 	var About = (props) => {
 		return (
-				<h2> About Component </h2>
+				<div>
+				<h1 className="text-center"> About </h1>
+				<p> This is a weather application build on React. </p>
+				<p> Here are some of the tools I used </p>
+				<ul>
+					<li>
+						<a href="https://facebook.github.io/react">React</a>
+					</li>
+					<li>
+						<a href="http://openweathermap.org">Open Weather Map</a>
+					</li>
+				</ul>
+				</div>
 			);
 	};
 
